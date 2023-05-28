8341bcc8f822219f5db370ff59624f898c7b876d
amine-asfar <74060479+amine-asfar@users.noreply.github.com>
2023-05-28 20:25:25 +0200

    improvement chatbot

--- src/screens/RendezVous.js
+++ src/screens/RendezVous.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 function RendezVous(customID) {
-
+  async function load_data() {
+    let { data: data, error } = await supabase.from("meezy").select("data")
+    console.log(data, error)
+  }
   
   return (
     <View>
